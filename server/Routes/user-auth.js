76b7bd2a87f7cfa2cc45dc30a6bdf38ab9fa0876
Sridar Saikrishnan <sridar.krishnan@preskale.com>
2024-05-05 14:19:59 +0530

    changes

--- server/Routes/user-auth.js
+++ server/Routes/user-auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../Controllers/userController");
 const { check } = require("express-validator");
-const {Validate} = require("../middleware/validate")
+const {Validate} = require("../Middleware/validate")
 
 router.post(
     "/sign-up",
